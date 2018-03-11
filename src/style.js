0e96a448723852d27e8f0fa69de64cce25da550f
Cuttle Cong <moyuyc95@gmail.com>
2018-03-11 23:35:22 +0800

    wrap style by .medium-image-progressive-container

--- src/style.js
+++ src/style.js
@@ -24,30 +24,34 @@ export default `
   -webkit-transition: visibility 0s linear 0s,opacity .4s 0s;
   transition: visibility 0s linear 0s,opacity .4s 0s;
 }
-.medium-image-progressive {
+.medium-image-progressive-container .medium-image-progressive {
   position: absolute;
   top:0;left:0;width:100%;height:100%;
   max-width: 100%;
   background-color: rgba(0, 0, 0, 0.05);
 }
-.medium-image-origin, .medium-image-progressive canvas {
+.medium-image-progressive-container .medium-image-origin,
+.medium-image-progressive-container .medium-image-progressive canvas {
   display: block;
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  margin: auto;
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
 }
-.canvas-loaded.medium-image-progressive:not(.image-loaded) canvas {
+.medium-image-progressive-container img, .medium-image-progressive-container canvas {
+  margin: 0 auto;
+}
+.medium-image-progressive-container .canvas-loaded.medium-image-progressive:not(.image-loaded) canvas {
   visibility: visible;
   opacity: 1;
   -webkit-transition: visibility 0s linear 0s,opacity .4s 0s;
   transition: visibility 0s linear 0s,opacity .4s 0s;
 }
-.medium-image-origin, .medium-image-progressive canvas {
+.medium-image-progressive-container .medium-image-origin,
+.medium-image-progressive-container .medium-image-progressive canvas {
   visibility: hidden;
   opacity: 0;
   -webkit-backface-visibility: hidden;
